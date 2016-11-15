af8c091a59a0cc5db8a76d204cb3184d020a1d8b
Lisa <Lisa@DESKTOP-TCI55VD>
2016-11-15 09:45:51 +0100

    Worker class done!

---
+++ Sandbox/Worker.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Worker : Employee
+    {
+        public int hourlyPay;
+        public int monthlyHours;
+
+        public Worker(string name, int hourlyPay, int monthlyHours) : base(name)
+        {
+            this.hourlyPay = hourlyPay;
+            this.monthlyHours = monthlyHours;
+        }
+
+        public override int GetBonusPerMonth()
+        {
+            return 0;
+        }
+
+        public override int GetSalaryPerMonth()
+        {
+            return hourlyPay * monthlyHours;
+        }
+
+        public override bool IsBonusPaidOut()
+        {
+            return false;
+        }
+    }
+}
