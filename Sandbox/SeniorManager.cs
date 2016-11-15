1b8b372b8ff0b354b136abd09044185fc818bb24
Lisa <Lisa@DESKTOP-TCI55VD>
2016-11-15 10:38:39 +0100

    SeniorManager done!

---
+++ Sandbox/SeniorManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class SeniorManager : Manager
+    {
+        private int performanceLevel;
+
+        public SeniorManager(string name, int monthlyBaseSalary, int monthlyBonus) : base(name, monthlyBaseSalary, monthlyBonus)
+        {
+            this.performanceLevel = 0;
+        }
+
+        public void SetPerformaceLevel(int performanceLevel)
+        {
+            this.performanceLevel = performanceLevel;
+        }
+
+        public override bool IsBonusPaidOut()
+        {
+            return (performanceLevel > 6);
+        }
+    }
+}
