06554e33757d09881748f881a42d745bf3d776c3
Lisa <Lisa@DESKTOP-TCI55VD>
2016-11-15 10:32:35 +0100

    JuniorManager done!

---
+++ Sandbox/JuniorManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class JuniorManager : Manager
+    {
+        // Instans fields
+        private int workedHours;
+
+        public JuniorManager(string name, int monthlyBaseSalary, int monthlyBonus,) : base(name, monthlyBaseSalary, monthlyBonus)
+        {
+            this.workedHours = 0;
+        }
+
+        public void SetWorkedHours(int workedHours)
+        {
+            this.workedHours = workedHours;
+        }
+        public override bool IsBonusPaidOut()
+        {
+            return (workedHours > 180);
+        }
+    }
+}
---
+++ Sandbox/Manager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sandbox
+{
+    class Manager : Employee
+    {
+        private int monthlyBaseSalary;
+        private int monthlyBonus;
+
+        public Manager(string name, int monthlyBaseSalary, int monthlyBonus) : base(name)
+        {
+            this.monthlyBaseSalary = monthlyBaseSalary;
+            this.monthlyBonus = monthlyBonus;
+        }
+
+        public override int GetBonusPerMonth()
+        {
+            return monthlyBonus;
+        }
+
+        public override int GetSalaryPerMonth()
+        {
+            int totalSalary = monthlyBaseSalary;
+
+            if (IsBonusPaidOut())
+            {
+                totalSalary = monthlyBaseSalary + monthlyBonus;
+            }
+            return totalSalary;
+        }
+    }
+}
